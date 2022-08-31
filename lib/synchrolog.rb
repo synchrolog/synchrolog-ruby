b5b68f5322e72649d37edbd8e8dfce9482e251f7
Roberto C. Rivera <roberto@devstud.io>
2022-08-31 01:18:28 -0400

    Fixed clip tracking middleware

--- lib/synchrolog.rb
+++ lib/synchrolog.rb
@@ -36,14 +36,11 @@ module Synchrolog
 
     class SynchrologFormatter < ActiveSupport::Logger::SimpleFormatter
       def call(severity, timestamp, progname, message)
-        anonymous_id_match = message.match(/\[synchrolog_anonymous_id:(\S*)\]/)
-        anonymous_id = anonymous_id_match.try(:[], 1)
         msg = message.dup
-        msg.slice!(anonymous_id_match.begin(0)..anonymous_id_match[0].length) if anonymous_id
-        user_id_match = msg.match(/\[synchrolog_user_id:(\S*)\]/)
-        user_id = user_id_match.try(:[], 1)
-        msg.slice!(user_id_match.begin(0)..user_id_match[0].length) if user_id
-        { type: severity, timestamp: timestamp.utc.iso8601(3), message: msg, anonymous_id: anonymous_id, user_id: user_id }
+        clip_id_match = msg.match(/\[synchrolog_clip_id:(\S*)\]/)
+        clip_id = clip_id_match.try(:[], 1)
+        msg.slice!(clip_id_match.begin(0)..clip_id_match[0].length) if clip_id
+        { type: severity, timestamp: timestamp.utc.iso8601(3), message: msg, clip_id: clip_id }
       end
     end
   end
--- lib/synchrolog/version.rb
+++ lib/synchrolog/version.rb
@@ -1,3 +1,3 @@
 module Synchrolog
-  VERSION = "0.1.8"
+  VERSION = "0.1.9"
 end
